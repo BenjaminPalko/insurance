ec6c61f49550dd6b987eb2b54fdfc79c0e24cca9
Benjamin <benjaminpalko@hotmail.com>
2023-06-02 17:51:59 -0400

    primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,13 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          main: '#4E7566'
+        }
+      }
     },
   },
   plugins: [],
