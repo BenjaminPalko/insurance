4bdf23c6e883c1dae21510022443ad19676359b6
Benjamin <benjaminpalko@hotmail.com>
2023-06-26 16:20:55 -0400

    env types for auth0

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,8 @@
 namespace NodeJS {
     interface ProcessEnv {
+        AUTH0_DOMAIN: string;
+        AUTH0_CLIENT_ID: string;
+        AUTH0_CLIENT_SECRET: string;
         NEXT_PUBLIC_HYGRAPH: string;
         NEXT_PUBLIC_HYGRAPH_ASSET_TOKEN: string;
     }
